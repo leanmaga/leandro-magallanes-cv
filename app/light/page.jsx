acc36f79b94a5e8f16e1da3084321509471bef85
leanmaga <thesharkcommunity@gmail.com>
2024-03-21 12:40:58 -0300

    responsive

--- app/light/page.jsx
+++ app/light/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaSun, FaMoon,  FaGamepad, FaBook,FaPhoneAlt, FaEnvelope, FaGlobe, FaLinkedin, FaMapMarkerAlt  } from 'react-icons/fa';
 
 const Home = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -16,7 +16,7 @@ const Home = () => {
         
         <div className={`hoja inset-0 bg-white shadow-lg rounded-lg w-[1200px] flex mt-10`}>
             
-            <div className={`left_Side ${darkMode ? 'bg-gray-900 text-white' : 'bg-[#003147] text-white'} p-10 relative z-10 w-[400px]`}>
+          <div className={`left_Side ${darkMode ? 'bg-gray-900 text-white' : 'bg-[#003147] text-white'} p-10 relative z-10 w-[400px]`}>
             
             <div className="profileText">
                 <div className="imgBx">
@@ -44,55 +44,58 @@ const Home = () => {
             </div>
 
             <div className="contactInfo mt-8">
-          <h3 className="title text-white">Contact Info</h3>
-          <ul>
-            <li className="flex items-center mt-4">
-              <span className="icon text-blue-400">
-                <i className="fas fa-phone"></i>
-              </span>
-              <span className="text text-white ml-2">+54 9 11 2776-4823</span>
-            </li>
-            <li className="flex items-center mt-4">
-              <span className="icon text-blue-400">
-                <i className="fas fa-envelope"></i>
-              </span>
-              <span className="text text-white ml-2">
-                <a
-                  href="mailto:leandro.magallanes@outlook.com?subject=Visualización CV&body=Hola Leandro Magallanes"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  leandro.magallanes@outlook.com
-                </a>
-              </span>
-            </li>
-            <li className="flex items-center mt-4">
-              <span className="icon text-blue-400">
-                <i className="fas fa-globe"></i>
-              </span>
-              <span className="text text-white ml-2">
-                <a href="https://www.leandro-magallanes.com.ar" target="_blank" rel="noopener noreferrer">
-                  www.leandro-magallanes.com.ar
-                </a>
-              </span>
-            </li>
-            <li className="flex items-center mt-4">
-              <span className="icon text-blue-400">
-                <i className="fab fa-linkedin"></i>
-              </span>
-              <span className="text text-white ml-2">
-                <a href="https://www.linkedin.com/in/leandromagallanes/" target="_blank" rel="noopener noreferrer">
-                  www.linkedin.com/in/leandromagallanes/
-                </a>
-              </span>
-            </li>
-            <li className="flex items-center mt-4">
-              <span className="icon text-blue-400">
-                <i className="fas fa-map-marker-alt"></i>
-              </span>
-              <span className="text text-white ml-2">Buenos Aires, Argentina</span>
-            </li>
-          </ul>
+              
+              <h3 className="title text-white">Contact Info</h3>
+              
+              <ul>
+                  <li className="flex items-center mt-4">
+                      <span className="icon text-blue-400">
+                          <FaPhoneAlt />
+                      </span>
+                      <span className="text text-white ml-2">+54 9 11 2776-4823</span>
+                  </li>
+                  <li className="flex items-center mt-4">
+                      <span className="icon text-blue-400">
+                          <FaEnvelope />
+                      </span>
+                      <span className="text text-white ml-2">
+                          <a
+                              href="mailto:leandro.magallanes@outlook.com?subject=Visualización CV&body=Hola Leandro Magallanes"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                          >
+                              leandro.magallanes@outlook.com
+                          </a>
+                      </span>
+                  </li>
+                  <li className="flex items-center mt-4">
+                      <span className="icon text-blue-400">
+                          <FaGlobe />
+                      </span>
+                      <span className="text text-white ml-2">
+                          <a href="https://www.leandro-magallanes.com.ar" target="_blank" rel="noopener noreferrer">
+                              www.leandro-magallanes.com.ar
+                          </a>
+                      </span>
+                  </li>
+                  <li className="flex items-center mt-4">
+                      <span className="icon text-blue-400">
+                          <FaLinkedin />
+                      </span>
+                      <span className="text text-white ml-2">
+                          <a href="https://www.linkedin.com/in/leandromagallanes/" target="_blank" rel="noopener noreferrer">
+                              www.linkedin.com/in/leandromagallanes/
+                          </a>
+                      </span>
+                  </li>
+                  <li className="flex items-center mt-4">
+                      <span className="icon text-blue-400">
+                          <FaMapMarkerAlt />
+                      </span>
+                      <span className="text text-white ml-2">Buenos Aires, Argentina</span>
+                  </li>
+              </ul>
+
             </div>
 
             <div className="contactInfo education mt-8">
@@ -121,7 +124,7 @@ const Home = () => {
                 </ul>
             </div>
 
-            <div className="contactInfo language mt-8">
+            <div className="contactInfo language mt-8 p-4">
                 <h3 className="title text-white">Language</h3>
                 <ul className="mt-4">
                     <li className="flex justify-between items-center mb-4">
@@ -139,136 +142,132 @@ const Home = () => {
                 </ul>
             </div>
 
+          </div>
 
+          <div className={`right_Side ${darkMode ? 'bg-[#003147] text-white' : 'bg-white text-black'} p-10 relative z-10 w-[800px]`}>
+              
+              <div className="about p-10">
+                  <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700  pb-5 uppercase`}>Profile</h2>
+                  <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>Desarrollador front-end con experiencia en la creación de interfaces dinámicas y atractivas. Con React, especialmente Next.js, he llevado a cabo proyectos innovadores que optimizan la experiencia del usuario y la velocidad de carga. Especializado en HTML, CSS, JavaScript, y frameworks como Bootstrap y Tailwind CSS para lograr diseños modernos y responsivos. Además, he explorado el mundo tridimensional con Three.js, llevando la creatividad a nuevas alturas.</p>
+              </div>
 
+              <div className="about p-10">
+                  
+                  <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase`}>Experience</h2>
 
-
-
-
-            </div>
-
-            <div className={`right_Side ${darkMode ? 'bg-[#003147] text-white' : 'bg-white text-black'} p-10 relative z-10 w-[800px]`}>
-               
-               <div className="about p-10">
-                    <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700  pb-5 uppercase`}>Profile</h2>
-                    <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>Desarrollador front-end con experiencia en la creación de interfaces dinámicas y atractivas. Con React, especialmente Next.js, he llevado a cabo proyectos innovadores que optimizan la experiencia del usuario y la velocidad de carga. Especializado en HTML, CSS, JavaScript, y frameworks como Bootstrap y Tailwind CSS para lograr diseños modernos y responsivos. Además, he explorado el mundo tridimensional con Three.js, llevando la creatividad a nuevas alturas.</p>
-                </div>
-
-                <div className="about p-10">
-                    
-                    <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase`}>Experience</h2>
-
-                    <div className="box">
-                      <div className="year_company m-2">
-                          <h5 className='text-[#03a9f4] uppercase'>2020 - Present</h5>
-                          <h5 >PatagoniaScript</h5>
-                      </div>
-                      <div className="text p-2">
-                          <h4 className='text-[#03a9f4] uppercase'>CEO & Founder</h4>
-                          <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}> Diseño de Landing Pages. Portafolios Digitales Personalizados. Landing Pages Temáticas para Eventos. Presentación de Productos a la Venta. Diseño de Sitios Web para Empresas. Optimización de Sitios para SEO. Desarrollo con Tecnologías Avanzadas. Asesoramiento en Estrategias de Marketing Digital. Soporte Continuo y Mantenimiento.  </p>
-                      </div>
-                    </div>
-                    
-                    <div className="box">
-                      <div className="year_company m-2">
-                          <h5 className='text-[#03a9f4] uppercase'>2012 - 2023</h5>
-                          <h5 >Brewda Construcciones</h5>
-                      </div>
-                      <div className="text p-2">
-                          <h4 className='text-[#03a9f4] uppercase'>Operario</h4>
-                          <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>Responsable de mantener un alto nivel de precisión en el manejo de efectivo y la administración de la caja. Coordinación y el manejo del estacionamie nto. Capacidad para manejar situaciones difíciles y tratar a los clientes con amabilidad y profesionalismo. Manejo de vehículos manuales, automáticos, híbridos y eléctricos.</p>
-                      </div>
-                    </div>
-
-                    <div className="box">
+                  <div className="box">
                     <div className="year_company m-2">
-                        <h5 className='text-[#03a9f4] uppercase'>2011 - 2012</h5>
-                        <h5>Hotel Park Hyatt</h5>
+                        <h5 className='text-[#03a9f4] uppercase'>2020 - Present</h5>
+                        <h5 >PatagoniaScript</h5>
                     </div>
                     <div className="text p-2">
-                        <h4 className='text-[#03a9f4] uppercase'>Valet Parking</h4>
-                        <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>He trabajado como especialista en servicio de valet parking en Park Hyatt durante un año. Mi trabajo consistía en recibir y saludar a los huéspedes de l hotel y asegurarme de que sus vehículos fueran estacionados de manera segura y eficiente en el estacionamiento del hotel. Además, gestionaba el cobro de las tarifas del estacionamiento y estaba disponible para brindar asistencia a los huéspedes que necesitaban ayuda con sus vehículos.</p>
+                        <h4 className='text-[#03a9f4] uppercase'>CEO & Founder</h4>
+                        <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}> Diseño de Landing Pages. Portafolios Digitales Personalizados. Landing Pages Temáticas para Eventos. Presentación de Productos a la Venta. Diseño de Sitios Web para Empresas. Optimización de Sitios para SEO. Desarrollo con Tecnologías Avanzadas. Asesoramiento en Estrategias de Marketing Digital. Soporte Continuo y Mantenimiento.  </p>
                     </div>
-                    </div>
-
-                </div>
-
-                <div className="about skills p-10">
-
-                  <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase` }>Professional Skills</h2>
+                  </div>
                   
                   <div className="box">
-
-                    <h4>HTML</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '95%' }}></div>
+                    <div className="year_company m-2">
+                        <h5 className='text-[#03a9f4] uppercase'>2012 - 2023</h5>
+                        <h5 >Brewda Construcciones</h5>
                     </div>
-
+                    <div className="text p-2">
+                        <h4 className='text-[#03a9f4] uppercase'>Operario</h4>
+                        <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>Responsable de mantener un alto nivel de precisión en el manejo de efectivo y la administración de la caja. Coordinación y el manejo del estacionamie nto. Capacidad para manejar situaciones difíciles y tratar a los clientes con amabilidad y profesionalismo. Manejo de vehículos manuales, automáticos, híbridos y eléctricos.</p>
+                    </div>
                   </div>
 
                   <div className="box">
-
-                    <h4>CSS3</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '80%' }}></div>
-                    </div>
-
+                  <div className="year_company m-2">
+                      <h5 className='text-[#03a9f4] uppercase'>2011 - 2012</h5>
+                      <h5>Hotel Park Hyatt</h5>
+                  </div>
+                  <div className="text p-2">
+                      <h4 className='text-[#03a9f4] uppercase'>Valet Parking</h4>
+                      <p className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700`}>He trabajado como especialista en servicio de valet parking en Park Hyatt durante un año. Mi trabajo consistía en recibir y saludar a los huéspedes de l hotel y asegurarme de que sus vehículos fueran estacionados de manera segura y eficiente en el estacionamiento del hotel. Además, gestionaba el cobro de las tarifas del estacionamiento y estaba disponible para brindar asistencia a los huéspedes que necesitaban ayuda con sus vehículos.</p>
+                  </div>
                   </div>
 
-                  <div className="box">
+              </div>
 
-                    <h4>JavaScript</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '60%' }}></div>
-                    </div>
+              <div className="about skills p-10">
 
-                  </div>
+                <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase` }>Professional Skills</h2>
+                
+                <div className="box">
 
-                  <div className="box">
-
-                    <h4>Bootstrap</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '90%' }}></div>
-                    </div>
-
-                  </div>
-
-                  <div className="box">
-
-                    <h4>Tailwind</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '90%' }}></div>
-                    </div>
-
-                  </div>
-
-                  <div className="box">
-
-                    <h4>React JS</h4>
-                    <div className="h-2 bg-blue-300 w-full relative">
-                        <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '60%' }}></div>
-                    </div>
-
+                  <h4>HTML</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '95%' }}></div>
                   </div>
 
                 </div>
 
-                <div className="about interest p-10">
+                <div className="box">
 
-                    <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase`}>Interest</h2>
-                    <ul>
-                      <li  className='text-[#03a9f4] uppercase'>
-                        <i className="fas fa-gamepad"></i>
+                  <h4>CSS3</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '80%' }}></div>
+                  </div>
+
+                </div>
+
+                <div className="box">
+
+                  <h4>JavaScript</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '60%' }}></div>
+                  </div>
+
+                </div>
+
+                <div className="box">
+
+                  <h4>Bootstrap</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '90%' }}></div>
+                  </div>
+
+                </div>
+
+                <div className="box">
+
+                  <h4>Tailwind</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '90%' }}></div>
+                  </div>
+
+                </div>
+
+                <div className="box">
+
+                  <h4>React JS</h4>
+                  <div className="h-2 bg-blue-300 w-full relative">
+                      <div className="absolute top-0 left-0 h-full bg-blue-500" style={{ width: '60%' }}></div>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className="about interest p-10">
+
+                  <h2 className={`${darkMode ? ' text-white' : ' text-black'} text-gray-700 pb-5 uppercase`}>Interest</h2>
+                  <ul>
+                        
+                    <li className="text-[#03a9f4] uppercase flex items-center">
+                        <FaGamepad className="mr-2 text-blue-500" />
                         Gaming
-                      </li>
-                      <li  className='text-[#03a9f4] uppercase'>
-                        <i className="fas fa-book"></i>
+                    </li>
+                    <li className="text-[#03a9f4] uppercase flex items-center">
+                        <FaBook className="mr-2 text-blue-500" />
                         Reading
-                      </li>
-                    </ul>
-                </div>
+                    </li>
+                </ul>
+      
+              </div>
 
-            </div> 
+          </div> 
 
         </div>
         
